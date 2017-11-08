28f9ccb1867c4507ac2ce132a49f722c0104ab4b
Alvaro Camillo Neto <alvaro.camillo@sp01.local>
2017-11-08 13:33:22 -0200

    Incluido modelo que herda de um modelo abstrato

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth.models import User
+
 
 
 class Subject(models.Model):
@@ -33,6 +36,46 @@ class Module(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
 
-    
+
     def __str__(self):
         return '{}. {}'.format(self.order, self.title)
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, related_name='contents')
+    content_type = models.ForeignKey(ContentType,
+                                     limit_choices_to={'model__in':('text',
+                                                                    'video',
+                                                                    'image',
+                                                                    'file')})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, related_name='%(class)s_related')
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    url = models.URLField()
